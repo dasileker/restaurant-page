da874bc154e861f11bc1f3a8c536d2a006241acb
zerradi <dasileker@gmail.com>
2021-02-28 13:56:44 +0100

    refactoring all

--- src/scripts/components/PageLoader.js
+++
@@ -1,7 +0,0 @@
-export const PageLoader = (container, section) => {
-  import(`./${section}`).then((module) => {
-    let sectionObject = module[section]();
-    container.innerHTML = sectionObject.html;
-    sectionObject.load();
-  });
-};
