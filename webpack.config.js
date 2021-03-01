3b1eb37271fe87b5a3ec69f72e62ad8884812517
zerradi <dasileker@gmail.com>
2021-03-01 14:06:41 +0100

    trying to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'doc'),
+    path: path.resolve(__dirname, 'docs'),
   },
   devServer: {
     contentBase: './dist',
